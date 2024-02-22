2f3b2cc93517f3bfa5ae06af94c52043a82c9063
Riken Shah <rikenshah.02@gmail.com>
2024-02-22 13:27:51 +0530

    fix: temp

--- web/src/app/gen/page.js
+++ web/src/app/gen/page.js
@@ -85,7 +85,7 @@ export default function App() {
         <div className="flex w-full flex-col m-4">
             {/* Add orgs credits here */}
             <Chip className="absolute top-2 right-2">Credits: {orgUser?.org?.credits ?? 0}</Chip>
-            <Tabs isDisabled={isDisabled} aria-label="Options" color="primary" variant="bordered" className="flex h-auto" selectedKey={selectedKey} onSelectionChange={setSelectedKey} >
+            <Tabs isDisabled={false} aria-label="Options" color="primary" variant="bordered" className="flex h-auto" selectedKey={selectedKey} onSelectionChange={setSelectedKey} >
                 <Tab
                     key="img2img"
                     title={
